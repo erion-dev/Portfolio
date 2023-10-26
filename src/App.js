7f7f9a774f4bde78ed0234dc22b4634006e1b3bf
Lirak Haxhikadrija <lirak.haxhikadrijawork@gmail.com>
2023-10-26 12:51:24 +0200

    Modify router

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Navbar from "./Pages/Home/NavBar";
 import Home from "./Pages/Home/Homescreen";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="*" element={<div>404 Not Found</div>}></Route>
-          </Routes>
-        </div>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<div>
+					<Navbar />
+					<Routes>
+						<Route path="portfolio/" element={<Home />}></Route>
+						<Route
+							path="portfolio/*"
+							element={<div>404 Not Found</div>}
+						></Route>
+					</Routes>
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
